a4f4a3c637f9b85306fe26d499925240f19de497
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-05 00:26:08 -0500

    added header, paragraph, start quiz button and updated css style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+var bigTitle = document.querySelector(".title");
+var rules = document.querySelector(".rules");
+
+bigTitle.textContent = "Coding Quiz Challenge";
+rules.textContent = "To complete this quiz please answer the following 5 questions within the limited timeframe of 1 minute (60 seconds). If any questions are incorrect, you are docked 10 seconds from the timer. If you reach 0 on the timer the quiz will end. Good luck!"
+
+// Create vars for all question headers and list answers
+// 
 // Timer for the whole quiz
 // Subtract time if question is wrong
 // Game over when all questions are reached or time = 0
