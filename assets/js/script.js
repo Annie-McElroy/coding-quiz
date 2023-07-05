5c96bebf42c1244cfd57c219621fc7285b93a218
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-05 17:21:11 -0500

    added highscore html, added flexbox to header to space-around the highscore link and timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var question = document.querySelector(".question");
 var choices = document.querySelector(".choiceList")
 
 bigTitle.textContent = "Coding Quiz Challenge";
-rules.textContent = "To complete this quiz please answer the following 5 questions within the limited timeframe of 1 minute (60 seconds). If any questions are incorrect, you are docked 10 seconds from the timer. If you reach 0 on the timer the quiz will end. Good luck!"
+rules.textContent = "To complete this quiz please answer the following 5 questions within the limited timeframe of 1 minute (60 seconds). If any questions are incorrect, you are docked 10 seconds from the timer. After answering all questions the quiz will end and you may store your score with your initials. If you reach 0 on the timer the quiz will end. Good luck!"
 
 // Create vars for all question headers and list answers
 // Timer for the whole quiz
